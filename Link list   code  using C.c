60779557be237214549d188b794158bc1a4c2ed2
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-11-10 21:24:03 +0600

    Update Link list   code  using C.c

--- Link list   code  using C.c
+++ Link list   code  using C.c
@@ -6,8 +6,22 @@ struct node next;
 };
 
 int main(){
-//code write here
+
+  struct node *a = null;
+  struct node *b = null;
+  struct node *c =null;
+
+  a =(struct node *) malloc (size of (struct node));
+  b =(struct node *) malloc (size of (struct node));
+  c =(struct node *) malloc (size of (struct node));
 
 
-return 0;
+    a-> data = 13;
+    b -> data = 15;
+    c -> data =29;
+      
+      
+      
+      
+      return 0;
 }
