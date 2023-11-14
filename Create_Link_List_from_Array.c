c9e14c2698fa0512ea7972e6549c548ce31492c6
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-11-14 11:07:02 +0600

    Update Create_Link_List_from_Array.c

--- Create_Link_List_from_Array.c
+++ Create_Link_List_from_Array.c
@@ -1,2 +1,8 @@
 #include<stdio.h>
-
+void createLinkList (int arr[], int size);
+int main(){
+  int a [] ={5,10,40};
+createLinkList (a,3);
+}
+void createLinkList (int arr [],int size){
+}                    // this is  a function skilition. 
