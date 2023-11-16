4d4fd21d01eb48a3838778e0537a33064f490572
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-11-16 10:37:34 +0600

    Update Create_Link_List_from_Array.c

--- Create_Link_List_from_Array.c
+++ Create_Link_List_from_Array.c
@@ -6,3 +6,9 @@ createLinkList (a,3);
 }
 void createLinkList (int arr [],int size){
 }                    // this is  a function skilition. 
+
+int i;
+for(i=0; i<size; i++)
+{
+printf("%d \n", arr[i]);
+}                           //hole code is a baby step in link list from array. 
