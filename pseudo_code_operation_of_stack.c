08c3a6b3a70684492ae3c44c180232a2e3c0d780
Hafizur Rahman <123953804+Hafizur-Rahman-SD@users.noreply.github.com>
2023-11-18 10:42:57 +0600

    Create pseudo_code_operation_of_stack.c

---
+++ pseudo_code_operation_of_stack.c
@@ -0,0 +1,29 @@
+// Write pseudo code for the 5 operations of stack
+//PUSH
+
+
+int A[15]     //create our array size. 
+
+//PUSH
+push(x){
+top <-top+1
+A[top] <-x
+}
+
+//POP
+pop(){
+top <-top -1
+}
+
+//TOP
+top(){
+return A[top]   //return the top value of Arrrays. 
+  }
+
+//ISEMPTY
+IsEmpty() {
+if (top == -1)
+  return true
+    else 
+return false
+  }
